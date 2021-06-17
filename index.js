6d3d5d34b8b0484fd95a7542ed161f3ff656209d
AlexC2103 <80043678+AlexC2103@users.noreply.github.com>
2021-06-17 12:50:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var gameMatrix = [];
-var chosenDifficulty = '';
+var boardSize = 0;
+var mines = 0;
 var openedCells = 0;
 
 function createButtons(difficulty) {
@@ -7,86 +8,72 @@ function createButtons(difficulty) {
   document.getElementById('intermediate').disabled = true;
   var clickedButtonId;
   if (difficulty === 'beginner') {
-    chosenDifficulty = difficulty;
-    clickedButtonId = 9;
+    mines = 11;
+    boardSize = 9;
   }
 
   if (difficulty === 'intermediate') {
-    chosenDifficulty = difficulty;
-    clickedButtonId = 16;
+    mines = 40;
+    boardSize = 16;
   }
 
   var boardStart = document.getElementById('appendGame');
-  if (clickedButtonId !== 'hard') {
-    for (var i = 0; i < parseInt(clickedButtonId); ++i) {
-      gameMatrix[i] = [];
-      for (var j = 0; j < parseInt(clickedButtonId); ++j) {
-        gameMatrix[i][j] = 0;
-      }
-    }
 
-    for (var i = 0; i < parseInt(clickedButtonId); ++i) {
-      for (var j = 0; j < parseInt(clickedButtonId); ++j) {
-        var button = document.createElement('BUTTON');
-        var buttonId = i * parseInt(clickedButtonId) + j + 1;
-        boardStart.appendChild(button);
-        button.setAttribute('class', 'boardButton');
-        button.setAttribute('id', buttonId);
-        button.setAttribute('onclick', 'gameCheck(this.id)');
-        button.setAttribute('oncontextmenu', 'flagStatus(this.id)');
-      }
-
-      var br = document.createElement('br');
-      boardStart.appendChild(br);
+  for (var i = 0; i < boardSize; ++i) {
+    gameMatrix[i] = [];
+    for (var j = 0; j < boardSize; ++j) {
+      gameMatrix[i][j] = 0;
     }
   }
 
-  placeMines(clickedButtonId);
-  defineCells(clickedButtonId);
+  for (var i = 0; i < boardSize; ++i) {
+    for (var j = 0; j < boardSize; ++j) {
+      var button = document.createElement('BUTTON');
+      var buttonId = i * boardSize + j + 1;
+      boardStart.appendChild(button);
+      button.setAttribute('class', 'boardButton');
+      button.setAttribute('id', buttonId);
+      button.setAttribute('onclick', 'gameCheck(this.id)');
+      button.setAttribute('oncontextmenu', 'flagStatus(this.id)');
+    }
+
+    var br = document.createElement('br');
+    boardStart.appendChild(br);
+  }
+
+  placeMines();
+  defineCells(boardSize);
 }
 
-function placeMines(difficulty) {
-  var boardSize = parseInt(difficulty);
-  if (boardSize === 9) {
-    for (var i = 1; i <= 11; ++i) {
-      var randomLine = Math.floor(Math.random() * 7) + 1;
-      var randomColumn = Math.floor(Math.random() * 7) + 1;
-      if (gameMatrix[randomLine][randomColumn] === 0) {
-        gameMatrix[randomLine][randomColumn] = 'mine';
-      } else {
-        while (gameMatrix[randomLine][randomColumn] !== 0) {
-          randomLine = Math.floor(Math.random() * 9);
-          randomColumn = Math.floor(Math.random() * 9);
-        }
+function placeMines() {
+  for (var i = 1; i <= mines; ++i) {
 
-        gameMatrix[randomLine][randomColumn] = 'mine';
-      }
-    }
-  } else if (boardSize === 16) {
-    for (var i = 1; i <= 40; ++i) {
-      var randomLine = Math.floor(Math.random() * 16);
-      var randomColumn = Math.floor(Math.random() * 16);
-      if (gameMatrix[randomLine][randomColumn] === 0) {
-        gameMatrix[randomLine][randomColumn] = 'mine';
-      } else {
-        while (gameMatrix[randomLine][randomColumn] !== 0) {
-          randomLine = Math.floor(Math.random() * 16);
-          randomColumn = Math.floor(Math.random() * 16);
-        }
+    var randomLine = Math.floor(Math.random() * boardSize);
+    var randomColumn = Math.floor(Math.random() * boardSize);
 
-        gameMatrix[randomLine][randomColumn] = 'mine';
+    if (gameMatrix[randomLine][randomColumn] === 0) {
+      gameMatrix[randomLine][randomColumn] = 'mine';
+    } else {
+      while (gameMatrix[randomLine][randomColumn] !== 0) {
+        randomLine = Math.floor(Math.random() * boardSize);
+        randomColumn = Math.floor(Math.random() * boardSize);
       }
+
+      gameMatrix[randomLine][randomColumn] = 'mine';
     }
   }
 }
 
-function defineCells(difficulty) {
-  for (var i = 0; i < difficulty; ++i) {
-    for (var j = 0; j < difficulty; ++j) {
+function defineCells() {
+  for (var i = 0; i < boardSize; ++i) {
+    for (var j = 0; j < boardSize; ++j) {
+
       if (gameMatrix[i][j] !== 'mine') {
+
         for (var line = i - 1; line <= i + 1; ++line) {
           for (var column = j - 1; column <= j + 1; ++column) {
-            if (line >= 0 && line < difficulty && column >= 0 && column < difficulty) {
+
+            if (line >= 0 && line < boardSize && column >= 0 && column < boardSize) {
               if (gameMatrix[line][column] === 'mine') {
                 gameMatrix[i][j]++;
               }
@@ -98,21 +85,11 @@ function defineCells(difficulty) {
   }
 }
 
-function gameCheck(clickedId, difficulty) {
+function gameCheck(clickedId) {
 
   var clickedCellId = parseInt(clickedId);
   if (document.getElementById(clickedCellId).disabled === false) {
     openedCells++;
-  }
-
-  var boardSize;
-  var mines;
-  if (chosenDifficulty === 'beginner') {
-    boardSize = 9;
-    mines = 11;
-  } else {
-    boardSize = 16;
-    mines = 40;
   }
 
   if (openedCells === boardSize * boardSize - mines) {
@@ -134,19 +111,24 @@ function gameCheck(clickedId, difficulty) {
     }
 
     alert('Game lost! Refresh to try again.');
+
   } else if (gameMatrix[line][column] !== 0) {
+
     document.getElementById(clickedCellId).innerHTML = gameMatrix[line][column];
     document.getElementById(clickedCellId).disabled = true;
-    colorCell(line, column, boardSize);
+    colorCell(clickedCellId);
+
   } else {
+
     document.getElementById(clickedCellId).disabled = true;
     gameMatrix[line][column] = '';
+
     for (var i = line - 1; i <= line + 1; ++i) {
       for (var j = column - 1; j <= column + 1; ++j) {
         if (i >= 0 && i < boardSize && j >= 0 && j < boardSize) {
           if (gameMatrix[i][j] !== 0) {
             document.getElementById(boardSize * i + j + 1).innerHTML = gameMatrix[i][j];
-            colorCell(i, j, boardSize);
+            colorCell(boardSize * i + j + 1);
           }
 
           if (document.getElementById(boardSize * i + j + 1).disabled === false) {
@@ -159,41 +141,35 @@ function gameCheck(clickedId, difficulty) {
 
       /// Recursion for opening a mine-free block
       if (column < boardSize - 1) {
-        gameCheck((clickedCellId + 1).toString(), difficulty);
+        gameCheck((clickedCellId + 1).toString());
       }
 
       if (column > 0) {
-        gameCheck((clickedCellId - 1).toString(), difficulty);
+        gameCheck((clickedCellId - 1).toString());
       }
 
       if (line > 0) {
-        gameCheck((clickedCellId - boardSize).toString(), difficulty);
+        gameCheck((clickedCellId - boardSize).toString());
       }
 
       if (line < boardSize - 1) {
-        gameCheck((clickedCellId + boardSize).toString(), difficulty);
+        gameCheck((clickedCellId + boardSize).toString());
       }
     }
   }
 }
 
-function revealMines(difficulty) {
-  for (var i = 0; i < difficulty; ++i) {
-    for (var j = 0; j < difficulty; ++j) {
+function revealMines() {
+  for (var i = 0; i < boardSize; ++i) {
+    for (var j = 0; j < boardSize; ++j) {
       if (gameMatrix[i][j] === 'mine') {
-        document.getElementById(difficulty * i + j + 1).style.backgroundColor = 'red';
+        document.getElementById(boardSize * i + j + 1).style.backgroundColor = 'red';
       }
     }
   }
 }
 
 function flagStatus(clickedCellId) {
-  var boardSize;
-  if (chosenDifficulty === 'beginner') {
-    boardSize = 9;
-  } else {
-    boardSize = 16;
-  }
 
   if (document.getElementById(clickedCellId).innerHTML === '') {
     document.getElementById(clickedCellId).innerHTML = '<i class="fas fa-flag"></i>';
@@ -202,13 +178,13 @@ function flagStatus(clickedCellId) {
       document.getElementById(clickedCellId).innerHTML = '';
     }
   }
+}
+
+function colorCell(clickedCellId) {
 
   var line = parseInt((clickedCellId - 1) / boardSize);
   var column = clickedCellId - line * boardSize - 1;
-}
 
-function colorCell(line, column, boardSize) {
-  var clickedCellId = boardSize * line + column + 1;
   if (gameMatrix[line][column] === 1) {
     document.getElementById(clickedCellId).style.color = 'blue';
   }
@@ -241,4 +217,3 @@ function colorCell(line, column, boardSize) {
     document.getElementById(clickedCellId).style.color = 'gray';
   }
 }
-
